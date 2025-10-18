57deffa8755c0f39b35f8077e7d3cbe1ae75abcb
Preston Arnold <me@prestonarnold.uk>
2025-10-18 22:29:08 +0100

    Update Scheduler to use Storage

--- crates/scheduler/src/lib.rs
+++ crates/scheduler/src/lib.rs
@@ -1,15 +1,18 @@
 use futures::stream::{FuturesUnordered, StreamExt};
 use rocky_core::{Job, JobWorker};
+use rocky_storage::Storage;
 use std::sync::Arc;
 
-pub struct Scheduler<W: JobWorker + 'static> {
+pub struct Scheduler<W: JobWorker + 'static, S: Storage + 'static> {
     worker: Arc<W>,
+    storage: Arc<S>,
 }
 
-impl<W: JobWorker + 'static> Scheduler<W> {
-    pub fn new(worker: W) -> Self {
+impl<W: JobWorker + 'static, S: Storage + 'static> Scheduler<W, S> {
+    pub fn new(worker: W, storage: S) -> Self {
         Self {
             worker: Arc::new(worker),
+            storage: Arc::new(storage),
         }
     }
 
@@ -18,8 +21,12 @@ impl<W: JobWorker + 'static> Scheduler<W> {
 
         for job in jobs {
             let worker = Arc::clone(&self.worker);
+            let storage = Arc::clone(&self.storage);
             futures.push(async move {
                 let result = worker.execute(&job).await;
+                if let Ok(ref r) = result {
+                    let _ = storage.save_result(r).await;
+                }
                 (job.id.clone(), result)
             });
         }
@@ -33,9 +40,7 @@ impl<W: JobWorker + 'static> Scheduler<W> {
                         serde_json::to_string_pretty(&result.output).unwrap()
                     );
                 }
-                Err(err) => {
-                    eprintln!("Job {} failed: {}", job_id, err);
-                }
+                Err(err) => eprintln!("Job {} failed: {}", job_id, err),
             }
         }
     }
